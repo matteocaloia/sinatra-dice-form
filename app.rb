7662df1f4345a2105c5e17400dbb67eb52a43e47
matteocaloia <164690390+matteocaloia@users.noreply.github.com>
2024-04-11 21:20:31 +0000

    push.commit done

--- app.rb
+++ app.rb
@@ -6,4 +6,29 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+
+  erb(:main)
+
 end
+
+
+get("/process_roll") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+  @num_dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+
+
+end
+
